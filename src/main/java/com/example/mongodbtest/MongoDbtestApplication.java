0945c7a873e17c43dd47688b43df27f9be41e782
yashen97 <yashenraveesha97@gmail.com>
2021-10-26 14:33:26 +0530

    c

---
+++ src/main/java/com/example/mongodbtest/MongoDbtestApplication.java
@@ -0,0 +1,15 @@
+package com.example.mongodbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class MongoDbtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbtestApplication.class, args);
+    }
+
+}
